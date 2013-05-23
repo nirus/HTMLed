a4ff5b0145b9643ffd492eb9f6bb8efc47be03ae
unknown <m1021210@A5MD15450.mindtree.com>
2013-05-23 14:56:24 +0530

    ver 0.1
    Conflicts:
    	index.html
    	js/HTMLed.js

--- js/HTMLed.js
+++ js/HTMLed.js
@@ -1,6 +1,6 @@
 
 
-/* 			HTMLed.js  ver 0.1
+/*          HTMLed.js  ver 0.1
  * library written by Niranjan Kumar
  * this library requires Jquery to be loaded first
  * If you are using jquery mobile framework this script should be loaded before jquery mobile
@@ -13,61 +13,43 @@
 
     
 $(document).ready(function(){   
-	
-	var xmlhttp;
-	//Your render settings should be mentioned here
-	var lang = {
-			autoDetect: false,	//This tells the browser to detect the language preference and load ".properties" file automatically
-			langPref: "fr",		// if you set "autoDetect:false" you should mention the name of the ".properties" file you want to load 
-			langBundle:"bundle",//folder name that contains all ".properties" files
-	};
-	
-	
-	if(lang.autoDetect)
-		{
-			lang.langPref = lang.langBundle.toString()+'/'+(window.navigator.language|| navigator.language ||navigator.userLanguage).toString()+'.properties';
-			//$('html').attr('lang',(window.navigator.language|| navigator.language ||navigator.userLanguage).toString());
-			document.getElementsByTagName('html')[0].setAttribute('lang',(window.navigator.language|| navigator.language ||navigator.userLanguage).toString());
-			console.log(lang.langPref);
-		}
-	else
-		{
-			//$('html').attr('lang',lang.langPref.toString());
-			document.getElementsByTagName('html')[0].setAttribute('lang',lang.langPref.toString());
-			lang.langPref = lang.langBundle.toString()+'/'+lang.langPref+'.properties';
-			console.log(lang.langPref);
-		}
-	
-	//The ".properties" file is read as a json file
-
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp = new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-
-xmlhttp.open("GET",lang.langPref,false);
-xmlhttp.send();
-xmlhttp.onreadystatechange = function()
-  {
-  	//console.log(xmlhttp.readyState,xmlhttp.status);
-  if (xmlhttp.readyState!=4)
-    {
-    	if(xmlhttp.status!=200)
-    	alert('unable to load the Language Bundle');
+    
+    //Your render settings should be mentioned here
+    var lang = {
+            autoDetect: false,  //This tells the browser to detect the language preference and load ".properties" file automatically
+            langPref: "fr",     // if you set "autoDetect:false" you should mention the name of the ".properties" file you want to load 
+            langBundle:"bundle",//folder name that contains all ".properties" files
     }
-  else{
-    	
-    	var data = xmlhttp.responseText;
-    	/*
-    	var regex = new RegExp('\\b'+key.toString()+'\\b','g');
-    	document.body.innerHTML = document.body.innerHTML.replace(regex, 'hi');
-    	*/
-    	console.log(data);
-    	
-    }
-  };
- });
+    
+    
+    if(lang.autoDetect)
+        {
+        
+        lang.langPref = lang.langBundle.toString()+'/'+(window.navigator.language|| navigator.language ||navigator.userLanguage).toString()+'.properties';
+        $('html').attr('lang',(window.navigator.language|| navigator.language ||navigator.userLanguage).toString());
+        console.log(lang.langPref);
+        }
+    else
+        {
+        $('html').attr('lang',lang.langPref.toString());
+        lang.langPref = lang.langBundle.toString()+'/'+lang.langPref+'.properties';
+        console.log(lang.langPref);
+        }
+    
+    //The ".properties" file is read as a json file
+$.ajax({
+        url:lang.langPref,
+        dataType:'JSON',
+        'global': false,
+        'async': false,
+        success: function(data){
+        
+        $.each(data, function(key, val) {
+            
+            var regex = new RegExp('\\b'+key.toString()+'\\b','g');
+            $('body').html( $('body').html().replace(regex,val.toString()));    //replaces the strings in the html file
+            
+            });
+      }
+});
+}).trigger('refresh');  //update the page after the change
