6067c1bdd89698e3079e2bd53b4cfc2079461a1e
nirus <nirus@live.in>
2014-01-15 14:46:36 +0530

    HTMLed is function. can be called as a function with global params.
    //Thanks to Tim smith for Suggestion

--- js/HTMLed.js
+++ js/HTMLed.js
@@ -10,46 +10,74 @@
  * goto http://jsfiddle.net/nirus/9FVTf/ to get the file name you want to set based on your browser eg: en-US.properties for english US language
  * contact me for any quieries : nirus@live.in
 */
-
-    
-$(document).ready(function(){   
-    
-    //Your render settings should be mentioned here
-    var lang = {
-            autoDetect: false,  //This tells the browser to detect the language preference and load ".properties" file automatically
-            langPref: "fr",     // if you set "autoDetect:false" you should mention the name of the ".properties" file you want to load 
-            langBundle:"bundle",//folder name that contains all ".properties" files
-    }
-    
-    
-    if(lang.autoDetect)
-        {
-        
-        lang.langPref = lang.langBundle.toString()+'/'+(window.navigator.language|| navigator.language ||navigator.userLanguage).toString()+'.properties';
-        $('html').attr('lang',(window.navigator.language|| navigator.language ||navigator.userLanguage).toString());
-        console.log(lang.langPref);
-        }
-    else
-        {
-        $('html').attr('lang',lang.langPref.toString());
-        lang.langPref = lang.langBundle.toString()+'/'+lang.langPref+'.properties';
-        console.log(lang.langPref);
-        }
-    
-    //The ".properties" file is read as a json file
-$.ajax({
-        url:lang.langPref,
-        dataType:'JSON',
-        'global': false,
-        'async': false,
-        success: function(data){
-        
-        $.each(data, function(key, val) {
             
-            var regex = new RegExp('\\b'+key.toString()+'\\b','g');
-            $('body').html( $('body').html().replace(regex,val.toString()));    //replaces the strings in the html file
-            
-            });
-      }
-});
-}).trigger('refresh');  //update the page after the change
+$(document).ready(  
+    window.HTMLed = function(global){   
+        if(typeof(global) == "function"){           
+            var global = this.global = window.global;
+         }
+        if((global != undefined) && (global != null)){
+                                     
+                var lang = global.options;
+                    //Your render settings should be mentioned here
+                    if(lang.autoDetect)
+                        {
+                        
+                        lang.langPref = lang.langBundle.toString()+'/'+(window.navigator.language|| navigator.language ||navigator.userLanguage).toString()+'.properties';
+                        $('html').attr('lang',(window.navigator.language|| navigator.language ||navigator.userLanguage).toString());
+                        console.log(lang.langPref);
+                        }
+                    else
+                        {
+                        $('html').attr('lang',lang.langPref.toString());
+                        lang.langPref = lang.langBundle.toString()+'/'+lang.langPref+'.properties';
+                        console.log(lang.langPref);
+                        }
+            if((global.externalPage == undefined) || (global.externalPage == null)) {    
+                    //The ".properties" file is read as a json file
+                $.ajax({
+                        url:lang.langPref,
+                        dataType:'JSON',
+                        'global': false,
+                        'async': false,
+                        success: function(data){
+                        
+                        $.each(data, function(key, val) {
+                            
+                            var regex = new RegExp('\\b'+key.toString()+'\\b','g');
+                            $('body').html( $('body').html().replace(regex,val.toString()));    //replaces the strings in the html file
+                            
+                            });
+                      }
+                });
+            }else{                
+                $.ajax({
+                        url:global.externalPage,
+                        dataType:'html',
+                        'global': false,
+                        'async': false,
+                        success: function(html){
+                            //var html = this.html;
+                            
+                                 $.ajax({
+                                        url:lang.langPref,
+                                        dataType:'JSON',
+                                        'global': false,
+                                        'async': false,
+                                        success: function(data){                                        
+                                            $.each(data, function(key, val) {                                            
+                                                var regex = new RegExp('\\b'+key.toString()+'\\b','g');
+                                                html = html.replace(regex,val.toString());                                               
+                                              });
+                                            global.callBack(html);
+                                        }
+                                   });                                                                         
+                         }
+                });
+                
+            }       
+     }else{
+            console.Error("Define global variable");
+           }
+   }
+).trigger('refresh');  //update the page after the change
